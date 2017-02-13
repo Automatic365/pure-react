5cc0f527a37a846176d7bbb4955893ded3b2c193
Jason Hanna <jason.hanna01@gmail.com>
2017-02-13 16:51:14 -0700

    updated string

--- greeting/src/index.js
+++ greeting/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
 var Greeting = React.createClass({
   getUsername: function(name){
     if(name){
-      return "Hello" + " " + name + "!"
+      return `Hello ${name}!`
     } else {
       return "Not logged in"
     }
@@ -14,7 +13,7 @@ var Greeting = React.createClass({
   render: function(){
     return(
       <div>
-         {this.getUsername(false)}
+        {this.getUsername("root")}
       </div>
     )
   }
